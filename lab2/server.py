e410fe1ea4a4ceb1be806fdc85a56698d980477d
losthope <arsen.melnuchyk@gmail.com>
2023-10-15 19:56:06 +0300

    upload lab2(CGI)

---
+++ lab2/server.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+addr = ("localhost", 9999)
+
+httpd = HTTPServer(addr, CGIHTTPRequestHandler)
+print(f"=== Server was started on {addr} ===")
+httpd.serve_forever()
+
+
+
