2c8fe691768fc8c4926e8d7c7e9fd390abc9be38
losthope <arsen.melnuchyk@gmail.com>
2023-10-15 20:15:47 +0300

    Resolved conflicts

--- lab1/client.py
+++ lab1/client.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import socket
 import time
 
@@ -17,24 +16,3 @@ def send():
 
 
 send()
-
-=======
-import socket
-import time
-
-def send():
-    host="localhost"
-    port=65333
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        start_time = time.strftime("%H:%M:%S", time.localtime())
-        s.sendall(start_time.encode())
-        while True:
-            now = time.strftime("%y/%m/%d %H:%M:%S", time.localtime())
-            message = input(f"[{now}] > ")
-            s.sendall(message.encode())
-
-
-send()
->>>>>>> origin/main
