259023b4ab32b2283474e215c9aad60a8f1fb5a9
losthope <arsen.melnuchyk@gmail.com>
2023-10-20 15:47:20 +0300

    skills api now returns json instead of regular python dictionary

--- flask/app/api/skills.py
+++ flask/app/api/skills.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from app import app
 from data import data
 
+
 app.add_url_rule("/api/skills/", endpoint="get_skills")
 app.add_url_rule("/api/skills/<int:idx>", endpoint="get_skills")
 
@@ -9,5 +10,5 @@ app.add_url_rule("/api/skills/<int:idx>", endpoint="get_skills")
 def get_skills(idx=None) -> dict|int:
     my_skills = data.my_skills
     if idx is None:
-        return my_skills
-    return my_skills.get(idx)
+        return jsonify(my_skills)
+    return jsonify(my_skills.get(idx))
