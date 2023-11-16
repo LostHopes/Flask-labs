626949e21b7deceef4ec1acfe50264957cb79bf5
losthope <arsen.melnuchyk@gmail.com>
2023-11-16 15:29:26 +0200

    changed quotes in __tablename__

--- flask/app/models.py
+++ flask/app/models.py
@@ -8,7 +8,7 @@ from app.config import db
 # TODO: add flask-migration
 
 class Users(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     login = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
@@ -24,11 +24,17 @@ class Users(db.Model):
 
 
 class Todo(db.Model):
-    __tablename__ = 'todo'
+    __tablename__ = "todo"
     id = db.Column(db.SmallInteger, primary_key=True, autoincrement=True)
     task_name = db.Column(db.Text, nullable=False)
     task_status = db.Column(db.Enum("Planning", "Completed", "Disgarded"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
 
+class Skills(db.Model):
+    __tablename__ = "skills"
+    id = db.Column(db.SmallInteger, primary_key=True, autoincrement=True)
+    skill_name = db.Column(db.String, nullable=False)
+    skill_desc = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 # TODO: add user comments!
