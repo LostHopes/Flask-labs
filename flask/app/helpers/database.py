33624a24f577f282ab610a456e96ffcca72951b3
losthope <arsen.melnuchyk@gmail.com>
2023-11-16 15:32:30 +0200

    added some changes to database handler

--- flask/app/helpers/database.py
+++ flask/app/helpers/database.py
@@ -1,8 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
-from app.models import db
+from app.models import db, Todo, Users
 from app import app
 
 
 with app.app_context():
-    db.create_all()
+    db.create_all(bind_key=None)
     print("Database created")
+
+
+class HandleUsers(Users):
+    def __init__(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def add(self):
+        pass
+
+
+class HandleTodos(Todo):
+    def __init__(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def add(self):
+        pass
