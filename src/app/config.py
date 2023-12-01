e2e15600670b59684ddd385360096c418a07ff43
losthope <arsen.melnuchyk@gmail.com>
2023-12-01 22:09:36 +0200

    modified a few lines in config file

--- src/app/config.py
+++ src/app/config.py
@@ -7,15 +7,14 @@ class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = b"secretkey123"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///" + os.path.join(basedir, "db", "users.sqlite")
 
 
 class DevConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///" + os.path.join(basedir, "db", "users.sqlite")
 
 
 class ProdConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///" + os.path.join(basedir, "db", "users.sqlite")
+    DEVELOPMENT = False
+    DEBUG = False
