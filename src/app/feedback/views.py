a38b00bb2ea586348e634a2701ff38424a66ec0c
losthope <arsen.melnuchyk@gmail.com>
2023-12-04 10:10:47 +0200

    added support for comments in feedback, changed some styles in account page

--- src/app/feedback/views.py
+++ src/app/feedback/views.py
@@ -11,17 +11,25 @@ from app.helpers import feedback_db
 def feedbacks():
     title = "Feedback"
     form = FeedbackForm()
+    helper = feedback_db.FeedbackHelper()
+    comments = helper.show()
 
     if form.validate_on_submit():
         return redirect(url_for("feedback.add"))
 
 
-    return render_template("feedback.html", title=title, form=form)
+    return render_template("feedback.html", title=title, form=form, comments=comments)
 
 
 @feedback.route("/add", methods=["POST"])
 @login_required
 def add():
+    db = feedback_db.FeedbackHelper()
+    db.add(
+        request.form.get("comment"),
+        request.form.get("file"),
+        current_user.get_id()
+    )
     return redirect(url_for("feedback.feedbacks"))
 
 
--- src/app/helpers/feedback_db.py
+++ src/app/helpers/feedback_db.py
@@ -1,10 +1,20 @@
+from datetime import datetime
+
 from app.feedback.models import Feedback
+from app import db
 
 
 class FeedbackHelper(Feedback):
 
-    def add(self):
-        pass
+    def show(self):
+        feedbacks = Feedback.query.all()
+        return feedbacks
+
+    def add(self, comment, file, user_id):
+        comment_date = datetime.now().replace(microsecond=0)
+        feedback = Feedback(comment=comment, file=file, comment_date=comment_date, user_id=user_id)
+        db.session.add(feedback)
+        db.session.commit()
 
     def remove(self):
         pass
