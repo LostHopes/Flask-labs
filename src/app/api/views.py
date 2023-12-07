415296e7a741d97e16ea3b61cd012255c8542341
losthope <arsen.melnuchyk@gmail.com>
2023-12-07 16:49:51 +0200

    added support to return all todos as json

--- src/app/api/views.py
+++ src/app/api/views.py
@@ -2,11 +2,24 @@ from flask import jsonify
 
 from . import api
 from app.todo.models import Todo
+from app import db
 
 
 @api.route("/todos")
 def get_todos():
-    pass
+    todos = Todo.query.all()
+    todos_json = []
+    for todo in todos:
+        todo_dict = {
+            "id": todo.id,
+            "task": todo.task,
+            "status": todo.status,
+            "category": todo.category,
+            "user_id": todo.user_id
+        }
+        todos_json.append(todo_dict)
+    return jsonify(todos_json)
+
 
 @api.route("/todos", methods=["POST"])
 def create_task():
