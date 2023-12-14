1e235071a4254c5569f49b7aa7e6f1833cf0735a
losthope <arsen.melnuchyk@gmail.com>
2023-12-14 21:00:40 +0200

    added login_required to the create, update, delete routes

--- src/app/posts/views.py
+++ src/app/posts/views.py
@@ -1,8 +1,17 @@
 from flask import url_for, redirect, render_template
+from flask_login import login_required, current_user
 
+from app.helpers import posts_db
 from . import posts
 
+
+@posts.route("/list")
+def posts_list():
+    title = "Posts"
+    return render_template("posts.html", title=title)
+
 @posts.route("/create", methods=["POST"])
+@login_required
 def create():
     pass
 
@@ -13,10 +22,12 @@ def get_post(id=None):
 
 
 @posts.route("/<int:id>/update", methods=["POST"])
+@login_required
 def update(id=None):
     pass
 
 
 @posts.route("/<int:id>/delete", methods=["POST"])
+@login_required
 def delete(id=None):
     pass
