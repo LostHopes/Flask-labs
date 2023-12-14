60167413287c1681c0a0c68de0c4f40800d071d5
losthope <arsen.melnuchyk@gmail.com>
2023-12-14 20:57:24 +0200

    added init methods for PostsHelper

---
+++ src/app/helpers/posts_db.py
@@ -0,0 +1,14 @@
+from app.posts.models import Posts
+from app import db
+
+
+class PostsHelper(Posts):
+    
+    def create():
+        pass
+
+    def remove():
+        pass
+
+    def update():
+        pass
