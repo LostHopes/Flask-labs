c7375fd6c147cb3bf727a8dfe6e814ce5b370a1a
losthope <arsen.melnuchyk@gmail.com>
2023-12-15 12:10:46 +0200

    renamed PostsForm to WritePostForm, added values and validators to form

--- src/app/posts/forms.py
+++ src/app/posts/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
-class PostsForm(FlaskForm):
-    pass
+class WritePostForm(FlaskForm):
+    title = StringField("Post title", validators=[DataRequired(), Length(min=10, max=50)])
+    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=50, max=5000)], render_kw={"cols": "50", "rows": "15"})
+    submit = SubmitField("Publish")
