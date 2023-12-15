c342fb36bbdd96d8ab7478ca2c3a610be492cf66
losthope <arsen.melnuchyk@gmail.com>
2023-12-15 12:04:56 +0200

    added show method, implemented create method in PostsHelper

--- src/app/helpers/posts_db.py
+++ src/app/helpers/posts_db.py
@@ -3,9 +3,15 @@ from app import db
 
 
 class PostsHelper(Posts):
+
+    def show(self):
+        query = Posts.query.all()
+        return query
     
-    def create():
-        pass
+    def create(self, title, text, user_id):
+        post = Posts(title=title, text=text, user_id=user_id)
+        db.session.add(post)
+        db.session.commit()
 
     def remove():
         pass
