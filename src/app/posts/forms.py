39907497d4640f7d92843e5b184a0e0ac8ab5429
losthope <arsen.melnuchyk@gmail.com>
2023-12-17 12:42:51 +0200

    added EditPostForm

--- src/app/posts/forms.py
+++ src/app/posts/forms.py
@@ -7,3 +7,9 @@ class WritePostForm(FlaskForm):
     title = StringField("Post title", validators=[DataRequired(), Length(min=10, max=50)])
     text = TextAreaField("Post content", validators=[DataRequired(), Length(min=50, max=5000)], render_kw={"cols": "50", "rows": "15"})
     submit = SubmitField("Publish")
+
+
+class EditPostForm(FlaskForm):
+    title = StringField("Post title", validators=[DataRequired(), Length(min=10, max=50)])
+    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=50, max=5000)], render_kw={"cols": "50", "rows": "15"})
+    submit = SubmitField("Edit")
