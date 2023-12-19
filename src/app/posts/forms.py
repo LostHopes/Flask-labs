bdc5de5184592b10c5851c57c5a08041fdadac35
losthope <arsen.melnuchyk@gmail.com>
2023-12-19 16:30:07 +0200

    changed min characters for the post to 250

--- src/app/posts/forms.py
+++ src/app/posts/forms.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired, Length
 
 class WritePostForm(FlaskForm):
     title = StringField("Post title", validators=[DataRequired(), Length(min=10, max=50)])
-    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=50, max=5000)], render_kw={"cols": "50", "rows": "15"})
+    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=250, max=5000)], render_kw={"cols": "50", "rows": "15"})
     submit = SubmitField("Publish")
 
 
 class EditPostForm(FlaskForm):
     title = StringField("Post title", validators=[DataRequired(), Length(min=10, max=50)])
-    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=50, max=5000)], render_kw={"cols": "50", "rows": "15"})
+    text = TextAreaField("Post content", validators=[DataRequired(), Length(min=250, max=5000)], render_kw={"cols": "50", "rows": "15"})
     submit = SubmitField("Edit")
