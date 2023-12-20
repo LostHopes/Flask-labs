7ded4f46fadb70d710a16d0e7aa3375c929aee1b
losthope <arsen.melnuchyk@gmail.com>
2023-12-20 10:09:52 +0200

    implemented pagination in show method of PostsHelper

--- src/app/helpers/posts_db.py
+++ src/app/helpers/posts_db.py
@@ -1,3 +1,5 @@
+from flask import request
+
 from app.posts.models import Posts
 from app.user.models import Users
 from app import db
@@ -5,9 +7,10 @@ from app import db
 
 class PostsHelper(Posts):
 
-    def show(self):
-        query = db.session.query(Posts, Users).join(Users).all()
+    def show(self, page, max_items):
+        query = db.session.query(Posts, Users).join(Users).order_by(Posts.created_at.desc()).paginate(page=page, per_page=max_items)
         return query
+
 
     def get(self, id):
         post = Posts.query.filter_by(id=id).first()
