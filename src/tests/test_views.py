2f9ac35a2ada66e9acd9755fb5512e0b84168c72
losthope <arsen.melnuchyk@gmail.com>
2024-01-07 19:10:02 +0200

    added test_views to tests

---
+++ src/tests/test_views.py
@@ -0,0 +1,9 @@
+import pytest
+from datetime import datetime
+
+from tests.main import client, app
+
+
+# def test_home_page(app):
+#     response = client.get("/", content_type="html/text")
+#     assert response.status_code == 200
