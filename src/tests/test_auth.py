c21c62141fe6a6e1b2baad8a762986d05ca1efa6
losthope <arsen.melnuchyk@gmail.com>
2024-01-11 11:25:04 +0200

    added test auth

---
+++ src/tests/test_auth.py
@@ -0,0 +1,23 @@
+from tests.conftest import client
+from app.user.models import Users
+
+
+def test_register_user(client):
+    response = client.post("/register", json={
+        "name": "Test",
+        "surname": "User",
+        "login": "Test123",
+        "email": "test123@gmail.com",
+        "password": "password",
+        "confirm_password": "password",
+    })
+    assert response.status_code == 200
+
+
+def test_login_user(client):
+    response = client.post("/login", json={
+        "email": "test123@gmail.com",
+        "password": "password"
+    })
+    assert response.status_code == 200
+    
