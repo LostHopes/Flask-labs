497af0e21f4ecbe13ca7c83c494e0423df87c021
losthope <arsen.melnuchyk@gmail.com>
2024-01-11 16:40:25 +0200

    added a few views test, added response.get_data test

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -7,6 +7,7 @@ def test_home_page(client):
     """Test the root page of the site"""
     response = client.get("/")
     assert response.status_code == 200
+    assert "Home page" in response.get_data(as_text=True)
 
 
 def test_about_page(client):
@@ -15,3 +16,27 @@ def test_about_page(client):
     assert response.status_code == 200
 
 
+def test_account_page(client):
+    """"""
+    response = client.get("/account")
+    assert response.status_code == 302
+
+
+def test_users_page(client):
+    """"""
+    response = client.get("/users")
+    assert response.status_code == 302
+
+
+def test_todo_page(client):
+    """"""
+    response = client.get("/todo/list")
+    assert response.status_code == 302
+
+
+def test_not_found_page(client):
+    """Test page that not exists in the app"""
+    response = client.get("/foo")
+    assert response.status_code == 404
+    assert "Not Found" in response.get_data(as_text=True)
+    
