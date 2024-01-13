fbdba2f4202fcbf1be4fc1f1e46c0d5c4ae15c97
losthope <arsen.melnuchyk@gmail.com>
2024-01-14 01:04:26 +0200

    added and updated a few tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+
 from app import create_app, db
+from app.user.models import Users
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -7,9 +9,14 @@ def app():
     """Create app for testing"""
     app = create_app()
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    app.config["TESTING"] = True
+    ctx = app.test_request_context()
+    ctx.push()
     yield app
     with app.app_context():
         db.session.remove()
+    
+    ctx.pop()
 
 
 @pytest.fixture
@@ -18,6 +25,12 @@ def client(app):
     to pass as an argument to other tests"""
     with app.test_client() as test_client:
         yield test_client
+
+
+@pytest.fixture
+def runner(app):
+    yield app.test_cli_runner()
+    
 
 
 
--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,24 +1,54 @@
+from datetime import datetime
+from flask_login import login_user
+
 from tests.conftest import client
 from app.user.models import Users
 
 
 def test_register_user(client):
     """Test register user using register form"""
-    response = client.post("/register", json={
-        "name": "Test",
-        "surname": "User",
-        "login": "Test123",
-        "email": "test123@gmail.com",
-        "password": "password",
-        "confirm_password": "password",
-    })
+    data = {
+        "name": "Test123",
+        "surname": "User123",
+        "login": "Test123456",
+        "email": "test123456@gmail.com",
+        "password": "password123",
+        "confirm_password": "password123",
+    }
+    response = client.get("/register", data=data, follow_redirects=True)
     assert response.status_code == 200
 
 
 def test_login_user(client):
     """Test register user using login form"""
-    response = client.post("/login", json={
-        "email": "test123@gmail.com",
+    
+    data = {
+        "email": "test@example.com",
         "password": "password"
-    })
+    }
+    user = Users.query.filter_by(email="test@gmail.com").first()
+    login_user(user, remember=True)
+    response = client.get("/login", data=data, follow_redirects=True)
     assert response.status_code == 200
+    assert response.request.path == "/account"
+    assert b"Account info" in response.data
+    assert len(response.history) == 1
+
+
+def test_update_account(client):
+    response = client.get("/account", follow_redirects=True)
+    user = Users.query.filter_by(email="test@gmail.com").first()
+    user.about = "Test about me!"
+    assert response.status_code == 200
+    assert response.request.path == "/account"
+    assert b"Test about me!" in response.data
+
+
+def test_logout_user(client):
+    """Test logout user"""
+    response = client.post("/logout", follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == "/login"
+    assert len(response.history) == 1
+    assert B"You have successfully logged out" in response.data
+
