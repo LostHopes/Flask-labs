ba02f85d65c8e99aee436d4bf6330ab33ad809f6
losthope <arsen.melnuchyk@gmail.com>
2024-02-03 10:21:23 +0200

    added lab2 folder from main brach and

--- src/server.py
+++ src/server.py
@@ -1,40 +1,10 @@
-import socket
-import time
-import os
-import threading
+from http.server import HTTPServer, CGIHTTPRequestHandler
 
-def handle_client(conn, addr, users):
-    connection_time = time.strftime("%D - %H:%M:%S", time.localtime())
-    users.append(conn)
-    print(f'User {os.getlogin()} with ip {addr} connected to the server in [{connection_time}]\
-        \nUsers online: {len(users)}')
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            users.remove(conn)
-            print(f"User {os.getlogin()} with ip {addr} disconnected from the server\
-                \nUsers online: {len(users)}")
-            break
-        recv_time = time.strftime("%D - %H:%M:%S", time.localtime())
-        print(f'{os.getlogin()}: {data.decode()} [{recv_time}]')
-        for user in users:
-            if user != conn:
-                user.sendall(data)
-    conn.close()
+addr = ("localhost", 9999)
 
-def recv(host, port):
-    users = []
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen(5)
-        while True:
-            conn, addr = s.accept()
-            t = threading.Thread(target=handle_client, args=(conn, addr, users))
-            t.start()
+httpd = HTTPServer(addr, CGIHTTPRequestHandler)
+print(f"=== Server was started on {addr} ===")
+httpd.serve_forever()
 
-def main():
-    host = 'localhost'
-    port = 65333
-    recv(host, port)
 
-main()
+
